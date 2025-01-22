75d538f578e9148c6f951689397dab31b7f26f6e
gdudek <gdudek.dev@gmail.com>
2025-01-22 08:58:56 +0100

    Affichage conditionnel avec switch

--- exercices.js
+++ exercices.js
@@ -1,14 +1,40 @@
 // Définition de variables
-const currency="🪙";
-const nom_boutique="Le Grimmoire Enchanté";
-let nb_potions=10;
-let prix_potion_soin=2;
-let open=true ;
+const currency = "🪙";
+const nom_boutique = "Le Grimmoire Enchanté";
+const nom_sorcier="Henri PotdBeurre"
+let nb_potions = 10;
+let prix_potion_soin = 2;
+let open = true;
 
 //Affichage conditionnel
 
-if(open){
-    console.log(`Bienvenue dans la boutique ${nom_boutique} Aventurier!`)
-}else{
-    console.log(`La boutique ${nom_boutique} est fermée, revenez plus tard`)
+if (open) {
+  console.log(`Bienvenue dans la boutique ${nom_boutique} Aventurier!`);
+} else {
+  console.log(`La boutique ${nom_boutique} est fermée, revenez plus tard`);
+}
+
+// Affichage conditionnel avec switch
+
+console.log(
+  "Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ? 🤔",
+  "1. Le nom de la boutique",
+  "2. Le nom du Sorcier",
+  "3. Le prix d'un potion de soin",
+  "4. La quantité d'une potion de soin"
+);
+
+let user_choice = prompt(
+  "Rentrez le numéro entre 1 et 4, correspondant à votre choix"
+);
+user_choice = parseInt(user_choice);
+switch (user_choice) {
+  case 1:
+    console.log(`Le nom de la boutique est: ${nom_boutique}`);
+  case 2:
+    console.log(`Le nom du sorcier est: ${nom_sorcier}`);
+  case 3:
+    console.log(`Le prix d'une potion de soin est de : ${prix_potion_soin} ${currency}`);
+  case 4:
+    console.log(`La boutique a actuellement en stock: ${nb_potions} potion(s) de soin`);
 }
