f8660c7b69ce1e6218e53a5a0803c44926ff5edb
gdudek <gdudek.dev@gmail.com>
2025-01-22 08:48:26 +0100

    Affichage conditionnel

---
+++ exercices.js
@@ -0,0 +1,14 @@
+// Définition de variables
+const currency="🪙";
+const nom_boutique="Le Grimmoire Enchanté";
+let nb_potions=10;
+let prix_potion_soin=2;
+let open=true ;
+
+//Affichage conditionnel
+
+if(open){
+    console.log(`Bienvenue dans la boutique ${nom_boutique} Aventurier!`)
+}else{
+    console.log(`La boutique ${nom_boutique} est fermée, revenez plus tard`)
+}
