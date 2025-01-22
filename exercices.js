9d7f0af542fa884a3fca4b4c6f4cde68b2c24c29
gdudek <gdudek.dev@gmail.com>
2025-01-22 09:09:37 +0100

    Bourse de l'Aventurier

--- exercices.js
+++ exercices.js
@@ -3,7 +3,7 @@ const currency = "🪙";
 const nom_boutique = "Le Grimmoire Enchanté";
 const nom_sorcier = "Henri PotdBeurre";
 let nb_potions = 10;
-let prix_potion_soin = 2;
+let prix_potion_soin = 1;
 let open = true;
 
 // //Affichage conditionnel
@@ -47,8 +47,24 @@ let open = true;
 //     );
 // }
 
-//Calcul du prix total d'une commande de potion
+// //Calcul du prix total d'une commande de potion
 
 let quantite_potion_soin=prompt("Combien de potions de soin désirez-vous?");
 let prix_total=quantite_potion_soin*prix_potion_soin;
 console.log(`Prix de ${quantite_potion_soin} potions de soin: ${prix_total} ${currency} mon cher Aventurier. 💸.`)
+
+//Bourse de l'Aventurier 💰
+
+let stock_argent=20;
+
+if(prix_total>stock_argent){
+    console.log(`Désolé Aventurier, vous n'avez pas assez d'argent pour cela...`)
+}else{
+    if(quantite_potion_soin>nb_potions){
+        console.log(`Désolé Aventurier, nous n'avons pass suffisament de potions en stock`);
+    }else{
+        console.log(`Voici vos potions Aventurier! Merci pour votre achat!`);
+        stock_argent-=prix_total;
+        nb_potions-=quantite_potion_soin;
+    };
+};
